96574d69b8376a63c2dcbf311ae8c26086b7de76
patawang1 <patawang941@gmail.com>
2024-05-07 18:44:04 -0400

    Watermelon answer

---
+++ watermelon.cpp
@@ -0,0 +1,21 @@
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+  int w;
+
+  cin >> w;
+
+  if (w % 2 == 0 && w > 2) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+
+  return 0;
+}
