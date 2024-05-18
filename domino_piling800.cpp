dd89a906d5f89d59661a35871324d5296ea87e9f
patawang1 <patawang941@gmail.com>
2024-05-18 11:39:48 -0400

    d0min0 piling 800 init

--- domino_piling800.cpp
+++ domino_piling800.cpp
@@ -1,13 +1,19 @@
-#pragma GCC optimize ("O3")
+#pragma GCC optimize("O3")
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
 
-    
+  int a, b, area;
 
-    return 0;
+  cin >> a >> b;
+
+  area = a * b;
+
+  cout << area / 2;
+
+  return 0;
 }
